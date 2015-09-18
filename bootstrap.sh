aeecbd2d883b4a3c86c5d2cc151abbef36e73971
Vyacheslav Voronenko <git@voronenko.info>
2015-09-18 22:26:10 +0300

    chore (init sh)

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,15 +21,14 @@ sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install ansible
 
-curl -sSL https://raw.githubusercontent.com/Voronenko/dotfiles/master/dotfiles_rsa > ./dotfiles_id_rsa && chmod 600 ./dotfiles_rsa
-ssh-add ./dotfiles_rsa; git clone git@github.com:Voronenko/dotfiles.git; ssh-add -d ./dotfiles_rsa;
+curl -sSL https://raw.githubusercontent.com/Voronenko/dotfiles/master/dotfiles_rsa > ./dotfiles_rsa && chmod 600 ./dotfiles_rsa
+ssh-add ./dotfiles_rsa; git clone git@github.com:Voronenko/dotfiles.git;
 rm ./dotfiles_rsa
 
 
-curl -sSL https://raw.githubusercontent.com/Voronenko/ansible-developer_recipes/master/recipes_id_rsa > ./recipes_rsa && chmod 600 ./recipes_rsa
-ssh-add ./recipes_rsa; git clone git@github.com:Voronenko/ansible-developer_recipes.git; ssh-add -d ./recipes_rsa;
+curl -sSL https://raw.githubusercontent.com/Voronenko/ansible-developer_recipes/master/recipes_rsa > ./recipes_rsa && chmod 600 ./recipes_rsa
+ssh-add ./recipes_rsa; git clone git@github.com:Voronenko/ansible-developer_recipes.git; 
 rm ./recipes_rsa
-
 
 cd dotfiles
 
