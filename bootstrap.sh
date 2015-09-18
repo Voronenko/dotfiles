39a008da39303d0d1c3b3999debaf7f7574f88f4
Vyacheslav Voronenko <git@voronenko.info>
2015-09-18 22:11:51 +0300

    initial bootstraping file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+# curl -sSL http://bit.ly/slavkodotfiles > bootstrap.sh && chmod +x bootstrap.sh
+# ./bootstrap.sh
  
 sudo apt-get install git
 
@@ -17,10 +20,16 @@ sudo apt-get install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install ansible
- 
-git clone git@github.com:Voronenko/dotfiles.git
- 
-git clone git@github.com:Voronenko/ansible-developer_recipes.git
+
+curl -sSL https://raw.githubusercontent.com/Voronenko/dotfiles/master/dotfiles_rsa > ./dotfiles_id_rsa && chmod 600 ./dotfiles_rsa
+ssh-add ./dotfiles_rsa; git clone git@github.com:Voronenko/dotfiles.git; ssh-add -d ./dotfiles_rsa;
+rm ./dotfiles_rsa
+
+
+curl -sSL https://raw.githubusercontent.com/Voronenko/ansible-developer_recipes/master/recipes_id_rsa > ./recipes_rsa && chmod 600 ./recipes_rsa
+ssh-add ./recipes_rsa; git clone git@github.com:Voronenko/ansible-developer_recipes.git; ssh-add -d ./recipes_rsa;
+rm ./recipes_rsa
+
 
 cd dotfiles
 
