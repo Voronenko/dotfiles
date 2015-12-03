af68786a01b374b6a7f1875c5634a4af97d039f2
Vyacheslav Voronenko <git@voronenko.info>
2015-12-03 16:11:42 +0200

    Added one more file from atom to sync

--- workplace_init.sh
+++ workplace_init.sh
@@ -2,3 +2,7 @@
 
 mv ~/.atom/keymap.cson ~/.atom/keymap.cson.old || true
 ln -s $PWD/atom/keymap.cson ~/.atom/keymap.cson
+
+mv ~/.atom/config.cson ~/.atom/config.cson.old || true
+ln -s $PWD/atom/config.cson ~/.atom/config.cson
+
