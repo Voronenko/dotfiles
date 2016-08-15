f944c74c99ae72337c2bee43cc32524d00fd7cb1
Vyacheslav Voronenko <git@voronenko.info>
2016-08-15 17:49:34 +0300

    chore (3rd party plugins) docker-compose

--- workplace_init.sh
+++ workplace_init.sh
@@ -6,3 +6,10 @@ ln -s $PWD/atom/keymap.cson ~/.atom/keymap.cson
 mv ~/.atom/config.cson ~/.atom/config.cson.old || true
 ln -s $PWD/atom/config.cson ~/.atom/config.cson
 
+mkdir -p ~/.zsh/completion
+curl -L https://raw.githubusercontent.com/sdurrheimer/docker-compose-zsh-completion/master/_docker-compose > ~/.zsh/completion/_docker-compose
+exec $SHELL -l
+
+# and add
+#fpath=(~/.zsh/completion $fpath)
+#autoload -Uz compinit && compinit -i
