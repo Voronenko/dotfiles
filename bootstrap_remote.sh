43728a3d8aa994ab67503252ad3d5831ff8dd83b
Vyacheslav Voronenko <git@voronenko.info>
2017-12-18 14:16:52 +0100

    improve(dotfiles) simple remote shell batch

---
+++ bootstrap_remote.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# curl -sSL http://bit.ly/getmyshell > getmyshell.sh && chmod +x getmyshell.sh && ./getmyshell.sh
+
+if [ -e /usr/bin/yum ]
+then
+    pkgmanager=yum
+elif [ -e /usr/bin/apt ]
+then
+    pkgmanager=apt-get
+else
+    echo "No supported package manager"
+    exit 1
+fi
+
+sudo $pkgmanager -y install curl
+
+curl -sSL https://raw.githubusercontent.com/Voronenko/dotfiles/master/bootstrap.sh > ~/bootstrap.sh
+curl -sSL https://raw.githubusercontent.com/Voronenko/dotfiles/master/bashrc > ~/.bashrc
+curl -sSL https://raw.githubusercontent.com/Voronenko/dotfiles/master/zshrc > ~/.zshrc
+touch ~/.zshrc.local
+touch ~/bashrc.local
