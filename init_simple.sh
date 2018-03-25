5aaa70574771650789df9ee1505a03dd3b77e023
Vyacheslav Voronenko <git@voronenko.info>
2018-03-25 21:57:23 +0200

    improve(kubectl, hub)

--- init_simple.sh
+++ init_simple.sh
@@ -41,7 +41,7 @@ if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
 
     echo "Clone my oh-my-zsh repository from GitHub only if it isn't already present"
     if [[ ! -d $dir/oh-my-zsh/ ]]; then
-        git clone http://github.com/michaeljsmalley/oh-my-zsh.git
+        git clone https://github.com/robbyrussell/oh-my-zsh.git
     fi
     echo "Set the default shell to zsh if it isn't currently set to zsh"
     if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
