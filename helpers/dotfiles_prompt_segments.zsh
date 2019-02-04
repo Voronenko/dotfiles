d0a8978abcd553d35c4bd34978624049348316a5
Vyacheslav Voronenko <git@voronenko.info>
2019-02-04 22:19:37 +0100

    improve(segments) added current aws profile if any, and POC for toggl billing notifications

--- helpers/dotfiles_prompt_segments.zsh
+++ helpers/dotfiles_prompt_segments.zsh
@@ -36,8 +36,10 @@ prompt_dot_dck() {
 
 # * * * * * /usr/bin/python /usr/local/bin/toggl now > ~/.toggl_now
 prompt_dot_toggl() {
-  TOGGLE_ACTIVITY=$(cat ~/.toggl_now | awk 'BEGIN { FS="[ ]" } ; { print $2 }' )
-  "$1_prompt_segment" "$0" "$2" gray white "$(print_icon 'TODO_ICON') ${TOGGLE_ACTIVITY}" ''
+  if [[ -f ~/.toggl_now ]]; then
+    TOGGLE_ACTIVITY=$(cat ~/.toggl_now | awk 'BEGIN { FS="[ ]" } ; { print $2 }' )
+    "$1_prompt_segment" "$0" "$2" gray white "$(print_icon 'TODO_ICON') ${TOGGLE_ACTIVITY}" ''
+  fi
 }
 
 
