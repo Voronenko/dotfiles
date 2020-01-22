ca4c2c001458d9b605beabf00a5d323f400f58e5
Vyacheslav Voronenko <git@voronenko.info>
2020-01-22 13:26:28 +0100

    improve(vscode plugins) adjusted list of currently used vscode plugins

--- code/install_code_extensions.sh
+++ code/install_code_extensions.sh
@@ -1,3 +1,4 @@
+code --install-extension aaron-bond.better-comments
 code --install-extension alefragnani.numbered-bookmarks
 code --install-extension amazonwebservices.aws-toolkit-vscode
 code --install-extension chef-software.chef
@@ -13,6 +14,7 @@ code --install-extension EditorConfig.EditorConfig
 code --install-extension eg2.tslint
 code --install-extension evilz.vscode-reveal
 code --install-extension felixfbecker.php-debug
+code --install-extension formulahendry.code-runner
 code --install-extension heptio.jsonnet
 code --install-extension HookyQR.beautify
 code --install-extension humao.rest-client
@@ -20,11 +22,11 @@ code --install-extension jebbs.plantuml
 code --install-extension lextudio.restructuredtext
 code --install-extension mauve.terraform
 code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-python.anaconda-extension-pack
 code --install-extension ms-python.python
 code --install-extension ms-vscode-remote.remote-containers
 code --install-extension ms-vscode-remote.remote-ssh
 code --install-extension ms-vscode-remote.remote-ssh-edit
-code --install-extension ms-vscode-remote.remote-ssh-explorer
 code --install-extension ms-vscode-remote.remote-wsl
 code --install-extension ms-vscode-remote.vscode-remote-extensionpack
 code --install-extension ms-vscode.atom-keybindings
@@ -33,12 +35,15 @@ code --install-extension ms-vscode.cpptools
 code --install-extension ms-vscode.Go
 code --install-extension ms-vscode.mono-debug
 code --install-extension ms-vscode.powershell
+code --install-extension njpwerner.autodocstring
 code --install-extension openhab.openhab
 code --install-extension rebornix.ruby
 code --install-extension redhat.java
+code --install-extension redhat.vscode-yaml
 code --install-extension searKing.preview-vscode
 code --install-extension shardulm94.trailing-spaces
 code --install-extension vscode-icons-team.vscode-icons
 code --install-extension waderyan.gitblame
 code --install-extension wholroyd.jinja
+code --install-extension wingrunr21.vscode-ruby
 code --install-extension xrc-inc.jsonnet-formatter
