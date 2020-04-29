4e0969923d18f2905b2442599ad0331290687e2d
Vyacheslav Voronenko <git@voronenko.info>
2020-04-29 13:31:12 +0200

    chore(git config) adjusted cleaning for dotnet based projects

--- bin/get-k3s.sh
+++ bin/get-k3s.sh
@@ -2,6 +2,9 @@
 
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --no-deploy traefik" sh
 
+#Using Docker as the Container Runtime
+#K3s includes and defaults to containerd, an industry-standard container runtime.
+#If you want to use Docker instead of containerd then you simply need to run the agent with the --docker flag.
 
 # Install dashboard
 #https://rancher.com/docs/k3s/latest/en/installation/kube-dashboard/
