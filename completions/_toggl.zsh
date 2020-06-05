f6d7f279a38b259b7168264d96693ddd1601d7e8
Vyacheslav Voronenko <git@voronenko.info>
2020-06-05 21:47:59 +0200

    toggl completions now for py3

---
+++ completions/_toggl.zsh
@@ -0,0 +1,8 @@
+#compdef toggl
+_toggl() {
+  eval $(env COMMANDLINE="${words[1,$CURRENT]}" _TOGGL_COMPLETE=complete-zsh  toggl)
+}
+if [[ "$(basename -- ${(%):-%x})" != "_toggl" ]]; then
+  compdef _toggl toggl
+fi
+
