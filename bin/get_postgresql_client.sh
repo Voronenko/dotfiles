eced363f0681a739a666bd4c0c76c3afc9bf7c50
Vyacheslav Voronenko <git@voronenko.info>
2021-02-05 12:17:48 +0100

    improve(tools) added utility script to install postgres client

---
+++ bin/get_postgresql_client.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CLIENT_TOOLS_VERSION=${1:-12}
+
+# Create the file repository configuration:
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+
+# Import the repository signing key:
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+
+# Update the package lists:
+sudo apt-get update
+
+# echo install client tools:
+sudo apt-get install postgresql-client-${CLIENT_TOOLS_VERSION}
